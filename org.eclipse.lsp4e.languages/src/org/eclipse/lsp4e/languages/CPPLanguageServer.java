ba421f21a3768c4a9ed411ee742f4a27c9582a67
Marc-Andre Laperle <malaperle@gmail.com>
2017-02-16 15:08:06 -0500

    Fix compilation with lastest master of lsp4e
    Signed-off-by: Marc-Andre Laperle <malaperle@gmail.com>

--- org.eclipse.lsp4e.languages/src/org/eclipse/lsp4e/languages/CPPLanguageServer.java
+++ org.eclipse.lsp4e.languages/src/org/eclipse/lsp4e/languages/CPPLanguageServer.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -27,7 +28,7 @@ public class CPPLanguageServer extends ProcessStreamConnectionProvider {
 	}
 
 	@Override
-	public Object getInitializationOptions(String rootPath) {
+	public Object getInitializationOptions(URI rootPath) {
 		Map<String, Object> map = new HashMap<>();
 		map.put("css", true);
 		map.put("javascript", true);
